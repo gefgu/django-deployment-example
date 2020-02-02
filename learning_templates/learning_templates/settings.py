e65ec8e51a45146fa3b007adade559a9a22cd00b
Gustavo Henrique Bruno dos Santos <53129852+gefgu@users.noreply.github.com>
2020-02-02 11:29:23 -0300

    allowed_hosts

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -70,6 +70,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'learning_templates.wsgi.application'
 
+ALLOWED_HOSTS = ['gefgu.pythonanywhere.com']
+
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
